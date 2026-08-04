b9142dd6a3f9651358a7df86619b5a51d8eb7871
dipen-b23 <dipenbatharaju@gmail.com>
2026-08-04 23:52:02 +0530

    added bhavins blog post

--- src/assets/blogs.js
+++ src/assets/blogs.js
@@ -10,8 +10,8 @@ export const blogPosts = [
     name: "RNC Team",
   },
   {
-    title: "heyyyy",
-    body: "arnav is gay",
-    name: "RNC Team",
+    title: "YOLO Explained: The Faster, Easier Way to Build Computer Vision Projects",
+    body: "When I was working on my final project, I needed a computer vision model, but I ran into a wall: models like R-CNN were too slow and lacked flexibility.Then I found YOLO (You Only Look Once). Not only was it faster and more flexible, but it was also extremely beginner-friendly because it uses Python and has a much simpler syntax compared to other models.\nWhat Makes YOLO Unique?\nInstead of looking at an image as one large whole to find an object, YOLO splits the image into a grid of smaller regions. It then predicts the probability of an object existing within each of those smaller regions simultaneously, combining all of them to figure out exactly what and where the object is.\nWhy It Matters?\nWith how easy YOLO is to use and how quickly newer, better versions are being released, I think this model is here to stay. It\’s an incredibly handy tool—especially for anyone diving into computer vision for the first time.",
+    name: "Bhavin A",
   },
 ];
